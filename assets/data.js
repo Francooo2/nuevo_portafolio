1401b969aa2edd71eaae7032ecdb43da740cdaaf
Franco Palma <fpalmalopez2@gmail.com>
2022-04-19 09:04:50 -0400

    Portafolio terminado

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,47 @@
 export const projects = [
     {
-        image: "todolist.png",
-        description: "Aplicación de tareas, con inicio de sesión, y registro de usuarios en Node.js, a modo de práctica. Se utilizaron tecnologias tales como: HTML, CSS, JavaScript, MySQL,Node.js, JWT y DigitalOcean.",
-        demo: "https://www.meorganizo.digital/",
-        code: "https://github.com/Francooo2/meOrganizoApp"
+        image      : "todolist.png",
+        description: "Aplicación de tareas, con inicio de sesión, y registro de usuarios en Node.js, a modo de práctica. Se utilizaron tecnologias tales como: HTML, CSS, JavaScript, MySQL,Node.js, JWT, bcryptjs y DigitalOcean.",
+        demo       : "https://www.meorganizo.digital/",
+        code       : "https://github.com/Francooo2/meOrganizoApp",
+        private    : false,
+        btnCode    : true,
+        btnDemo    : true
+    },
+    {
+        image      : "restaurant.png",
+        description: "Aplicación realizada a restaurant Mutto's en Concepción, automatizando las operaciones diarias del local en áreas como: gestión de mesas, gestión de productos, gráficas y métricas para la toma de decisión, entre otras. Se utilizaron tecnologias tales como: HTML, CSS, JavaScript, MySQL, Node.js, JWT, bcryptjs, Socket.io, Chart.js y DigitalOcean.",
+        demo       : "https://www.meorganizo.digital/",
+        code       : "https://github.com/Francooo2/meOrganizoApp",
+        private    : true,
+        btnCode    : true,
+        btnDemo    : true
+    },
+    {
+        image      : "tienda.png",
+        description: "FrontEnd de tienda online que consume datos desde FakeApi Store de Platzi a modo de práctica. Se utilizaron tecnologias tales como: HTML, CSS, JavaScript, GitHub Pages.",
+        demo       : "https://francooo2.github.io/frontend_tienda/",
+        code       : "https://github.com/Francooo2/frontend_tienda",
+        private    : false,
+        btnCode    : true,
+        btnDemo    : true
+    },
+    {
+        image      : "reactcrud.png",
+        description: "Aplicación tipo CRUD con React y Node.js a modo de práctica. Se utilizaron tecnologias tales como: React, Node.js, Bootstrap, MySQL, Sequelize, Express.",
+        demo       : "https://francooo2.github.io/frontend_tienda/",
+        code       : "https://github.com/Francooo2/reactCRUD",
+        private    : false,
+        btnCode    : true,
+        btnDemo    : false
+    },
+    {
+        image      : "suricata.png",
+        description: "Maquetación de sitio web de compra de comidas, utilizando Bootstrap.",
+        demo       : "https://francooo2.github.io/suricata_page/",
+        code       : "https://github.com/Francooo2/suricata_page",
+        private    : false,
+        btnCode    : true,
+        btnDemo    : true
     }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 import { projects } from '../data.js'
 
-const profile         = document.getElementById('job_profile')
-const nav             = document.getElementById('principalnav')
-const menuBtn         = document.getElementById('btn')
-const iconSeparator   = document.getElementById('separator')
-const srctionProjects = document.getElementById('project')
+const profile = document.getElementById('job_profile')
+const nav = document.getElementById('principalnav')
+const menuBtn = document.getElementById('btn')
+const iconSeparator = document.getElementById('separator')
+const sectionProjects = document.getElementById('project')
+const body = document.getElementById('body')
+const home            = document.getElementById('home')
 
 setTimeout(() => {
-    profile.classList.add('showcontent')
-    nav.classList.add('showcontent')
-    iconSeparator.classList.add('showcontent')
+  profile.classList.add('showcontent')
+  nav.classList.add('showcontent')
+  iconSeparator.classList.add('showcontent')
+  body.style.overflowY = 'visible'
 }, 1000)
 
 window.onscroll = () => {
@@ -23,3 +26,55 @@ window.onscroll = () => {
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle('show')
 })
+
+home.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+})
+
+for (const project of projects) {
+  if (project.private === true) {
+    sectionProjects.innerHTML += `<article class="card">
+    <div class="card__header">
+        <img class="card__img" src="assets/img/${project.image}" alt="Imágen descriptiva">
+    </div>
+    <p class="card__description">${project.description}</p>
+    <div class="card__footer">
+    </div>
+  </article>`
+  } else if (project.btnCode === false) {
+    sectionProjects.innerHTML += `<article class="card">
+    <div class="card__header">
+        <img class="card__img" src="assets/img/${project.image}" alt="Imágen descriptiva">
+    </div>
+    <p class="card__description">${project.description}</p>
+    <div class="card__footer">
+        <a href="${project.demo}" target="_blank" class="card__demo">Demo</a>
+    </div>
+  </article>`
+  } else if (project.btnDemo === false) {
+    sectionProjects.innerHTML += `<article class="card">
+    <div class="card__header">
+        <img class="card__img" src="assets/img/${project.image}" alt="Imágen descriptiva">
+    </div>
+    <p class="card__description">${project.description}</p>
+    <div class="card__footer">
+        <a href="${project.code}" target="_blank" class="card__code">Código</a>
+    </div>
+  </article>`
+  } else {
+    sectionProjects.innerHTML += `<article class="card">
+    <div class="card__header">
+        <img class="card__img" src="assets/img/${project.image}" alt="Imágen descriptiva">
+    </div>
+    <p class="card__description">${project.description}</p>
+    <div class="card__footer">
+        <a href="${project.demo}" target="_blank" class="card__demo">Demo</a>
+        <a href="${project.code}" target="_blank" class="card__code">Código</a>
+    </div>
+  </article>`
+  }
+}
